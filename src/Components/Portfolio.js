94e2fe505c73e9c877e4738904d59d21c2cf8ef6
ArviRizqi <arvirf6013@gmail.com>
2025-07-02 09:28:39 +0700

    update

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,8 +13,10 @@ class Portfolio extends Component {
             return (
                 <div key={id++} className="columns portfolio-item">
                     <div className="item-wrap">
-                        <Zmage alt={projects.title} src={projectImage} />
-                        <div style={{textAlign: "center"}}>{projects.title}</div>
+                        <a href={projects.url} title={projects.title} target="_blank" rel="noopener noreferrer">
+                            <Zmage alt={projects.title} src={projectImage} />
+                            <div style={{textAlign: "center"}}>{projects.title}</div>
+                        </a>
                     </div>
                 </div>
             );
